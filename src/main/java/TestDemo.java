4fe832bbe2365b3b124a6c33c81ee0e8cfafb80a
Stephanie Curd <stephaniecurd@gmail.com>
2022-05-12 12:44:22 -0400

    Week 12 Coding Final Commit

---
+++ src/main/java/TestDemo.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class TestDemo {
+
+	public int addPositive(int a, int b) {
+		if (a > 0 && b > 0) {
+			return a + b;
+		} else {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+
+	}
+
+	int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	
+	}
+
+	public int randomNumberSquared() {
+		int randomNumber = getRandomInt();
+		return randomNumber * randomNumber;
+	
+	}
+}
+
+
+
